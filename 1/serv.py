ab41a7b30640bb680a6baa4401df1216af758e19
Christopher Randin <cdrandin@hotmail.com>
2014-11-08 19:10:18 -0800

    Getting client and server to communicate with each other. Also, commands are being send over and ls is being sent to client. Need to change it to include a data channel using a new unique port number

--- 1/serv.py
+++ 1/serv.py
@@ -2,34 +2,126 @@
 import socket
 import os
 import sys
+import cPickle as pickle
+import commands
+
+def enum(**enums):
+    return type('Enum', (), enums)
+
+COMMANDS = enum(UNDEFINED = 0, QUIT = 1, PUT = 2, GET = 3, LS = 4)
+
+# Get what was sent correctly. Returning what ever pickled object was sent over the network
+def unpackInfo(clientSocket):
+	# The temporary buffer to store the received
+	# data.
+	recvBuff = ''
+
+	# The size of the incoming file
+	fileSize = 0	
+
+	# The buffer containing the file size
+	fileSizeBuff = ''
+
+	# Receive the first 10 bytes indicating the
+	# size of the file
+	fileSizeBuff = recvAll(clientSocket, 10)
+		
+	# Get the file size
+	fileSize = int(fileSizeBuff)
+
+	#print 'The file size is ', fileSize
+
+	# Get the file data
+	info = recvAll(clientSocket, fileSize)
+
+	# unpickle object from returned pickled object
+	info = pickle.loads(info)
+
+	return info
+	
+# Get the buffered data given a specific number of bytes
+def recvAll(sock, numBytes):
+
+	# The buffer
+	recvBuff = ''
+	
+	# The temporary buffer
+	tmpBuff = ''
+	
+	# Keep receiving till all is received
+	while len(recvBuff) < numBytes:
+		
+		# Attempt to receive bytes
+		tmpBuff =  sock.recv(numBytes)
+		
+		# The other side has closed the socket
+		if not tmpBuff:
+			break
+		
+		# Add the received bytes to the buffer
+		recvBuff += tmpBuff
+	
+	return recvBuff
+
+# Pickle object. Include header length and send off to socket
+def sendInfo(clientSocket, info):
+	# Pickle object
+	info = pickle.dumps(info, -1) 
+
+	dataSizeStr = str(len(info))
+
+	# Append length of the data as header in front
+	while len(dataSizeStr) < 10:
+		dataSizeStr = '0' + dataSizeStr
+
+	info = dataSizeStr + info
+
+	# Keep sending
+	bytesSent = 0
+	while len(info) > bytesSent:
+		bytesSent += clientSocket.send(info[bytesSent:])
+
+
+def lsOutput():
+	return [line for line in commands.getstatusoutput('ls -l')]
 
 def main():
 	port = (int)(sys.argv[1])
 	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	serverSocket.bind(('', port))
+	serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	serverSocket.bind(('localhost', port))
 	serverSocket.listen(1)
 
-	print("Server ready")
+	print('Server ready')
 	while 1:
-		connectionSocket , addr = serverSocket.accept()
+		clientSocket , addr = serverSocket.accept()
 
-		tmpBuff = ""
+		print 'Accepted connection from client: ', addr
 
-		while len(data) is not 40:
-			tmpBuff = connectionSocket.recv(40)
+		cont = True
+		while cont:
+			info = unpackInfo(clientSocket)
 
-			if not tmpBuff:
-				break
+			print 'The file data is: '
+			print info
 
-			data += tmpBuff
+			# Quit
+			if info['command'] is COMMANDS.QUIT:
+				print 'Quiting with ', addr
+				clientSocket.close()
+				cont = False
 
-		print data
-		connectionSocket.close()
-
+			# Didn't quit so respond to client with info
+			else:
+				if info['command'] is COMMANDS.LS:
+					sendInfo(clientSocket, lsOutput())
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
-		print "USAGE: python %s <port_number>" %(sys.agv[0])
+		print 'USAGE: python %s <port_number>' %(sys.agv[0])
 		exit()
 
-	main()
+	try:
+		main()
+	except KeyboardInterrupt:
+		print "\nKeyboardInterrupt: Quiting silently"
